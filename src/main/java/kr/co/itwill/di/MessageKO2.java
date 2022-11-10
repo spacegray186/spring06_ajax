c453032c2d81f15333100e76106405ca1dcd7ada
spacegray186 <109955351+spacegray186@users.noreply.github.com>
2022-11-10 10:30:40 +0900

    Day071_221108

--- src/main/java/kr/co/itwill/di/MessageKO2.java
+++ src/main/java/kr/co/itwill/di/MessageKO2.java
@@ -6,11 +6,11 @@ public class MessageKO2 implements IHello {
 
     public MessageKO2() {
         System.out.println("-----MessageKO2() 객체 생성");
-      }
+    }
     
     @Override
     public void sayHello(String name) {
-      JOptionPane.showMessageDialog(null, "안녕하세요~~" + name);
+      JOptionPane.showMessageDialog(null, "안녕하세요" + name);
     }
     
 }//class end
